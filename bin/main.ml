c99e7aea2bfa9b2a7d8dcd9d73bd50a5549729ab
THCSC <tobias@skancode.dk>
2024-11-28 15:23:52 +0100

    add shared types module

--- bin/main.ml
+++ bin/main.ml
@@ -1,29 +1,71 @@
-type command =
-  | Register of string option
-  | Unknown
+open Shared.Types
+open Cli_2fa
 
-let display_usage ?(msg="") () = (
-  Printf.printf "%s\n\n" msg;
-  Printf.printf "Usage: cli_2fa [command] [options]\n\n";
+let display_usage ?(msg="") ?(name="cli_2fa") () = (
+  if String.length msg > 0 then
+    Printf.printf "%s\n\n" msg;
+  
+  Printf.printf "Usage: %s [command] [options]\n\n" name;
   Printf.printf "Commands:\n";
   Printf.printf "  register -r [key] : Register a new key for TOTP\n";
-  Printf.printf "  help -h           : Prints this message\n"
+  Printf.printf "  help -h           : Prints this message\n\n";
+  Printf.printf "Options:\n";
+  Printf.printf "  verbose -v        : Output all info\n"
 )
 
+let _debug_context (ctx:context) = 
+  Printf.printf "Context:\n";
+  Printf.printf "  cmd: %s\n" (
+    match ctx.cmd with
+        | Unknown -> "unknown"
+        | Register k -> (
+          match k with 
+              | Some k -> "register with '" ^ k ^ "'"
+              | None -> "register with no key"
+        )
+  );
+  Printf.printf "  verbose: %s\n" (
+    match ctx.verbose with
+    | true -> "true"
+    | false -> "false"
+  )
+
+let run (ctx:context) =
+  match ctx.cmd with
+  | Unknown -> failwith "Got a unknown command, this should never happen!"
+  | Register k -> Command.register_key k ctx
+
 let () = 
-  let rec parse_args (argv:string list) acc =
+  let rec parse_args (argv:string list) (acc:context) =
     match argv with
     | [] -> acc
     | arg::rest -> 
         match arg with
         | "register" | "-r" -> (
-          match rest with
-          | [] -> parse_args rest (Register None)
-          | k::rest -> parse_args rest (Register (Some k))
+          if acc.cmd <> Unknown then (
+            display_usage ~msg:"Too many commands found. You can only perform 1 at a time" ();
+            exit 1
+          ) else (
+            match rest with
+            | [] -> parse_args rest ({
+              cmd = Register None;
+              verbose = acc.verbose;
+            })
+            | k::rest -> parse_args rest ({
+              cmd = Register (Some k);
+              verbose = acc.verbose;
+            })
+          )
         );
+        | "verbose" | "-v" -> (
+          parse_args rest {
+            cmd = acc.cmd;
+            verbose = true;
+          }
+        )
         | "help" | "-h" -> (
           display_usage ();
-          exit 1
+          exit 0
           )
         | _ -> (
           display_usage ~msg:("Unknown command: " ^ arg) ();
@@ -34,12 +76,9 @@ let () =
   let argv = Sys.argv in
 
   match Array.to_list argv with
-  | [] -> display_usage ()
-  | [_] -> display_usage ()
+  | []  -> display_usage ()
+  | [name] -> display_usage ~name:name ()
   | _ :: args ->  
-    let cmd = parse_args args Unknown in
-    Printf.printf "Found command: %s\n" (
-      match cmd with
-      | Register _ -> "register"
-      | Unknown -> "unknown"
-    );
+      let ctx = parse_args args {cmd = Unknown; verbose = false;} in
+      _debug_context ctx;
+      run ctx
