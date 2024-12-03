e5b17984601bf83d7c2a624cb8ac05f918de16ae
TobiasTheDanish <t.hc199811@gmail.com>
2024-12-03 23:10:39 +0100

    more ansi escape codes

--- lib/ansi.ml
+++ lib/ansi.ml
@@ -1,5 +1,63 @@
 open Printf
 
+type graphic_mode = 
+  | Bold
+  | Dim
+  | Italic
+  | Underline
+  | Blinking
+  | Reverse
+  | Hidden
+  | StrikeThrough
+  | Reset
+
+let mode_to_int = function
+  | Bold -> 1
+  | Dim -> 2
+  | Italic -> 3
+  | Underline -> 4
+  | Blinking -> 5
+  | Reverse -> 7
+  | Hidden -> 8
+  | StrikeThrough -> 9
+  | Reset -> 0
+
+type color = 
+  | Black
+  | Red 
+  | Green 
+  | Yellow 
+  | Blue
+  | Magenta
+  | Cyan 
+  | White
+  | Default
+  | Reset
+
+let fg_color_to_int = function
+  | Black   -> 30
+  | Red     -> 31
+  | Green   -> 32
+  | Yellow  -> 33
+  | Blue    -> 34
+  | Magenta -> 35
+  | Cyan    -> 36
+  | White   -> 37
+  | Default -> 39
+  | Reset   -> 0
+
+let bg_color_to_int = function
+  | Black   -> 40
+  | Red     -> 41
+  | Green   -> 42
+  | Yellow  -> 43
+  | Blue    -> 44
+  | Magenta -> 45
+  | Cyan    -> 46
+  | White   -> 47
+  | Default -> 49
+  | Reset   -> 0
+
 let escape_char = "\x1B"
 
 let clear_screen () = 
@@ -9,7 +67,7 @@ let move_cursor_home () =
   printf "%s[H" escape_char
 
 let move_cursor (row:int) (col:int) =
-  printf "%s[%d;%dH" escape_char row col
+  printf "%s[%d;%df" escape_char row col
 
 let move_cursor_up (rows:int) = 
   printf "%s[%dA" escape_char rows
@@ -29,9 +87,18 @@ let move_cursor_up_start (rows:int) =
 let move_cursor_down_start (rows:int) = 
   printf "%s[%dE" escape_char rows
 
+let move_cursor_to_col (col:int) =
+  printf "%s[%dG" escape_char col
+
 let output (text:string) =
   printf "%s" text
 
 let output_line (text:string) =
   output text;
   move_cursor_down_start 1
+
+let set_color (mode:graphic_mode) (fg:color) (bg:color) =
+  printf "%s[%d;%d;%dm" escape_char (mode_to_int mode) (fg_color_to_int fg) (bg_color_to_int bg)
+
+let set_graphic_mode (mode:graphic_mode) = 
+  printf "%s[%dm" escape_char (mode_to_int mode)
--- lib/ansi.mli
+++ lib/ansi.mli
@@ -1,3 +1,26 @@
+type graphic_mode = 
+  | Bold
+  | Dim
+  | Italic
+  | Underline
+  | Blinking
+  | Reverse
+  | Hidden
+  | StrikeThrough
+  | Reset
+
+type color = 
+  | Black
+  | Red 
+  | Green 
+  | Yellow 
+  | Blue
+  | Magenta
+  | Cyan 
+  | White
+  | Default
+  | Reset
+
 val clear_screen : unit -> unit
 val move_cursor_home: unit -> unit
 val move_cursor : int -> int -> unit
@@ -7,6 +30,10 @@ val move_cursor_right : int -> unit
 val move_cursor_left : int -> unit
 val move_cursor_up_start : int -> unit
 val move_cursor_down_start : int -> unit
+val move_cursor_to_col : int -> unit
 
 val output : string -> unit
 val output_line : string -> unit
+
+val set_color : graphic_mode -> color -> color -> unit
+val set_graphic_mode : graphic_mode -> unit
