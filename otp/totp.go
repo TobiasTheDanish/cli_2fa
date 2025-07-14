737596492579c26a126a3eea56fae24c3105bd7b
TobiasTheDanish <t.hc199811@gmail.com>
2025-07-14 20:01:44 +0200

    feat: remove error from generateTotp

--- otp/totp.go
+++ otp/totp.go
@@ -6,16 +6,16 @@ import (
 	"fmt"
 )
 
-func generateTotp(key string, t uint64, step, digits int) (string, error) {
+func generateTotp(key string, t uint64, step, digits int) string {
 	counter := t / uint64(step)
 
 	decoded, err := base32.StdEncoding.DecodeString(key)
 	if err != nil {
-		return "", err
+		decoded = []byte(key)
 	}
 	hexKey := hex.EncodeToString(decoded)
 
 	hotp := generateHOTP(hexKey, counter, digits)
 
-	return fmt.Sprintf("%0*d", digits, hotp), nil
+	return fmt.Sprintf("%0*d", digits, hotp)
 }
--- otp/totp_test.go
+++ otp/totp_test.go
@@ -9,81 +9,66 @@ var rfcTestKey = "12345678901234567890"
 
 func TestGenerateTotp(t *testing.T) {
 	tests := []struct {
-		name    string
-		key     string
-		t       uint64
-		step    int
-		digits  int
-		want    string
-		wantErr bool
+		name   string
+		key    string
+		t      uint64
+		step   int
+		digits int
+		want   string
 	}{
 		{
-			name:    "RFC 6238 Test case 1",
-			key:     base32.StdEncoding.EncodeToString([]byte(rfcTestKey)),
-			t:       59,
-			step:    30,
-			digits:  8,
-			want:    "94287082",
-			wantErr: false,
+			name:   "RFC 6238 Test case 1",
+			key:    base32.StdEncoding.EncodeToString([]byte(rfcTestKey)),
+			t:      59,
+			step:   30,
+			digits: 8,
+			want:   "94287082",
 		},
 		{
-			name:    "RFC 6238 Test case 2",
-			key:     base32.StdEncoding.EncodeToString([]byte(rfcTestKey)),
-			t:       1111111109,
-			step:    30,
-			digits:  8,
-			want:    "07081804",
-			wantErr: false,
+			name:   "RFC 6238 Test case 2",
+			key:    base32.StdEncoding.EncodeToString([]byte(rfcTestKey)),
+			t:      1111111109,
+			step:   30,
+			digits: 8,
+			want:   "07081804",
 		},
 		{
-			name:    "RFC 6238 Test case 3",
-			key:     base32.StdEncoding.EncodeToString([]byte(rfcTestKey)),
-			t:       1111111111,
-			step:    30,
-			digits:  8,
-			want:    "14050471",
-			wantErr: false,
+			name:   "RFC 6238 Test case 3",
+			key:    base32.StdEncoding.EncodeToString([]byte(rfcTestKey)),
+			t:      1111111111,
+			step:   30,
+			digits: 8,
+			want:   "14050471",
 		},
 		{
-			name:    "RFC 6238 Test case 4",
-			key:     base32.StdEncoding.EncodeToString([]byte(rfcTestKey)),
-			t:       1234567890,
-			step:    30,
-			digits:  8,
-			want:    "89005924",
-			wantErr: false,
+			name:   "RFC 6238 Test case 4",
+			key:    base32.StdEncoding.EncodeToString([]byte(rfcTestKey)),
+			t:      1234567890,
+			step:   30,
+			digits: 8,
+			want:   "89005924",
 		},
 		{
-			name:    "RFC 6238 Test case 5",
-			key:     base32.StdEncoding.EncodeToString([]byte(rfcTestKey)),
-			t:       2000000000,
-			step:    30,
-			digits:  8,
-			want:    "69279037",
-			wantErr: false,
+			name:   "RFC 6238 Test case 5",
+			key:    base32.StdEncoding.EncodeToString([]byte(rfcTestKey)),
+			t:      2000000000,
+			step:   30,
+			digits: 8,
+			want:   "69279037",
 		},
 		{
-			name:    "RFC 6238 Test case 6",
-			key:     base32.StdEncoding.EncodeToString([]byte(rfcTestKey)),
-			t:       20000000000,
-			step:    30,
-			digits:  8,
-			want:    "65353130",
-			wantErr: false,
+			name:   "RFC 6238 Test case 6",
+			key:    base32.StdEncoding.EncodeToString([]byte(rfcTestKey)),
+			t:      20000000000,
+			step:   30,
+			digits: 8,
+			want:   "65353130",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, gotErr := generateTotp(tt.key, tt.t, tt.step, tt.digits)
-			if gotErr != nil {
-				if !tt.wantErr {
-					t.Errorf("GenerateTotp() failed: %v", gotErr)
-				}
-				return
-			}
-			if tt.wantErr {
-				t.Fatal("GenerateTotp() succeeded unexpectedly")
-			}
+			got := generateTotp(tt.key, tt.t, tt.step, tt.digits)
+
 			if got != tt.want {
 				t.Errorf("GenerateTotp() = %v, want %v", got, tt.want)
 			}
